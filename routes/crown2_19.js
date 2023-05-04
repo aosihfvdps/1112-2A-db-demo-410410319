f300744d0c3392cd5723e09813d13e2545767975
aosihfvdps <410410319@gms.tku.edu.tw>
2023-05-04 17:27:41 +0800

    today is May 4th, 17:27

--- routes/crown2_19.js
+++ routes/crown2_19.js
@@ -17,6 +17,23 @@ router.get('/', async function (req, res, next){
     }     
 });
 
+router.get('/shop2_19/', async function (req, res, next){
+    try{
+        let results = await db.query(`
+            select * from shop2_19`);
+        console.log('results', JSON.stringify(results.rows));
+        //res.json(results.rows);
+        res.render('crown2_19/shop2_19', {
+            data: results.rows,
+            category: "All products",
+            name: 'Chen Ji Wei', 
+            ID: '410410319'
+        }) 
+    } catch(error){
+        console.log(error);
+    }
+});
+
 
 router.get('/shop2_19/:category', async function (req, res, next){
     console.log('category', req.params.category)
@@ -28,13 +45,13 @@ router.get('/shop2_19/:category', async function (req, res, next){
             and C.name = $1
         `, [req.params.category]);
         console.log('results', JSON.stringify(results.rows));
-        res.json(results.rows);
-       /*  res.render('crown2_19/shop2_19', {
+        //res.json(results.rows);
+        res.render('crown2_19/shop2_19', {
             data: results.rows,
             category: req.params.category,
             name: 'Chen Ji Wei', 
             ID: '410410319'
-        }) */
+        }) 
     } catch(error){
         console.log(error);
     }
